24a72e8029bc53ece020463adba9e5992ae34bce
mihalko274 <michal.michal160@gmail.com>
2022-07-15 11:03:45 +0200

    added hotel and updated homepage

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -12,8 +12,8 @@ export class OrderDetailsService {
   {
     id:"1",
     hotelName:"Iberotel Costa Mares",
-    hotelDescription:"test",
-    hotelImg:"",
+    hotelDescription:"Luxusný hotel Iberotel Costa Mares leží na krásnej piesočnatej pláži a úžasneho živého zálivu Marsa Mubarak. Moderne zariadený hotelový rezort s priateľskou rodinnou atmosférou ponúka nezabudnuteľné zážitky v podobe vynikajúcej kuchyne, šnorchlovania, potápania a relaxácie.",
+    hotelImg:"/assets/hotels_files/iberotel_costa_mares.jpg",
   }
 
   ]
