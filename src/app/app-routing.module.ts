19fc1448cdd6dffd505d43c5d2b025686fabf949
mihalko274 <michal.michal160@gmail.com>
2022-07-22 13:26:06 +0200

    AuthGuard adedd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HotelpageComponent } from './pages/hotelpage/hotelpage.component';
 import { HotelsComponent } from './pages/hotels/hotels.component';
 import { ReefsComponent } from './pages/reefs/reefs.component';
 
+
 const routes: Routes = [
-  {path:'',component:HomeComponent},
+  // {path:'',component:HomeComponent},
   {path:'hotels',component:HotelsComponent},
   {path:'hotels/:hotelName',component:HotelpageComponent},
   {path:'reefs',component:ReefsComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'contact',component:ContactComponent},
+  {path:'login',component:LoginComponent},
+  {path:'', redirectTo:'/login',pathMatch:'full'},
+  {path:'forgot-password',component:ForgotPasswordComponent},
+  {path:'not-found',component:NotFoundComponent},
+  {
+    path: 'admin',
+    loadChildren:() =>
+      import('./modules/admin/admin.module').then((m)=> m.AdminModule)}
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SwiperModule } from 'swiper/angular';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepages/navbar/navbar.component';
@@ -12,7 +10,11 @@ import { HotelsComponent } from './pages/hotels/hotels.component';
 import { ReefsComponent } from './pages/reefs/reefs.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HotelpageComponent } from './pages/hotelpage/hotelpage.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { AdminDashboardComponent } from './modules/admin/components/admin-dashboard/admin-dashboard.component';
+//import { HeaderComponent } from './modules/admin/components/header/header.component';
 
 
 
@@ -26,6 +28,11 @@ import { HotelpageComponent } from './pages/hotelpage/hotelpage.component';
     ReefsComponent,
     ContactComponent,
     HotelpageComponent,
+    LoginComponent,
+    NotFoundComponent,
+    ForgotPasswordComponent,
+    AdminDashboardComponent,
+ //   HeaderComponent,
   ],
   imports: [
     BrowserModule,
