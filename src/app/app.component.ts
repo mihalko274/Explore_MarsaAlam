3a7d8c1aa77b4329ded9bd0dba151e70c73ad482
mihalko274 <michal.michal160@gmail.com>
2022-07-27 08:37:40 +0200

    restore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,37 +7,4 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AppComponent {
   title = 'Explore_MarsaAlam';
-
-
-
-  public userArray: User[] = [];
-  constructor(private http: HttpClient){
-    this.http.get('/assets/hotels_files/datafeed_hotels.csv', {responseType: 'text'})
-    .subscribe(
-        data => {
-            let csvToRowArray = data.split("\n");
-            for (let index = 1; index < csvToRowArray.length; index++) {
-              let row = csvToRowArray[index].split(",");
-              this.userArray.push(new User( parseInt( row[0], 10), row[1], row[2].trim()));
-            }
-            console.log(this.userArray);
-        },
-        error => {
-            console.log(error);
-        }
-    );
-  }
-}
-
-export class User{
-  id: number;
-  name: String;
-  lastName: String;
-
-  constructor(id: number, name: String, lastName: String){
-    this.id = id;
-    this.name = name;
-    this.lastName = lastName;
-  }
-
 }
--- src/app/pages/hotels/hotels.component.ts
+++ src/app/pages/hotels/hotels.component.ts
@@ -1,34 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderDetailsService } from 'src/app/services/order-details.service';
-import { DatafeedHotelsService } from 'src/app/services/datafeed-hotels.service';
 
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
   styleUrls: ['./hotels.component.css']
 })
-
-
-
 export class HotelsComponent implements OnInit {
 
 
 
-  constructor(private datafeedhotelService:DatafeedHotelsService) { }
-  hotelDataV: any = [];
-
+  constructor(private service:OrderDetailsService) { }
+  hotelData:any;
   ngOnInit(): void {
-    this.getData();
-  }
-
-  getData(){
-    this.datafeedhotelService.getInfo().subscribe(data => {
-      const list = data.split('\n');
-      list.forEach(e => {
-        this.hotelDataV.push(e);
-      })
-    })
-
+    this.hotelData = this.service.hotelDetails;
   }
 
 }
