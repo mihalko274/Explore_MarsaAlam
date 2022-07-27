90b2a81de1729d23edeb14aa0cc1d86a1dd1247d
mihalko274 <michal.michal160@gmail.com>
2022-07-27 13:07:23 +0200

    respo admin navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { HttpClientModule } from '@angular/common/http'
     HotelpageComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
